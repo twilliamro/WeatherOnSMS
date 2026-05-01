f71bcd4445797d3e7da1c0c9be1499097d22311b
twilliamro <66513087+twilliamro@users.noreply.github.com>
2026-05-01 20:58:26 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+import os
+import requests
+from datetime import datetime, timezone
+from timezonefinder import TimezoneFinder
+import pytz
+from twilio.rest import Client
+
+##With Secrets
+#for Twilio
+# account_sid = os.environ["account_sid"]
+# auth_token = os.environ["auth_token"]
+# TwilioPhoneNumber = os.environ["TwilioPhoneNumber"]
+# MyPhoneNumber = os.environ["MyPhoneNumber"]
+#for weather information
+# API_key = os.environ["API_key"]
+# MY_LAT = os.environ["MY_LAT"]
+# MY_LONG = os.environ["MY_LONG"]
+
+
+## local usage
+#for Twilio
+account_sid = "AC9b6bcfd9af93e4428182f980c732c036"
+auth_token = "cdc78ef03f24bdbd1ca0c4f7e8b14280"
+TwilioPhoneNumber = "+13203734212"
+MyPhoneNumber = "+5511933246824"
+#for weather information
+API_key = 'c3c1c81b643d040d85bfe90663a6277d'
+MY_LAT = -23.567149
+MY_LONG = -46.648037
+
+#FindTimezone from Coordinates
+tf = TimezoneFinder()
+timezone_str = tf.timezone_at(lat=MY_LAT, lng=MY_LONG)
+print(f"Timezone: {timezone_str}")
+# Create timezone OBJECT from the string
+timezone_obj = pytz.timezone(timezone_str)
+
+parameters = {
+    "lat":MY_LAT,
+    "lon":MY_LONG,
+    "appid":API_key,
+    "units": "metric",
+    "cnt": 8,
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+# if id < 700: bring_umbrela: data['list'][0-3]['weather'][0]['id']
+for i, forecast in enumerate(data['list']):
+    weather_id = forecast['weather'][0]["id"]
+    if weather_id < 700:
+        weather_main = forecast['weather'][0]["main"]
+        dt_txt = forecast['dt_txt']
+
+        # Convert dt_txt to UTC datetime
+        utc_dt = datetime.fromisoformat(dt_txt)
+        utc_dt = pytz.UTC.localize(utc_dt)
+
+        #convert to local time usein timezoe object
+        local_dt = utc_dt.astimezone(timezone_obj)
+        #Call strftime() on the datetime object, NOT on the string
+        formatted_time = local_dt.strftime('%d/%m/%Y %H:%M:%S')
+
+        print(f"Take a Umbrela!!! at {formatted_time}, Weather: {weather_main}  ID= {weather_id}")
+
+#get complete weather forecast for timezone_str and present in timezone_str timezone
+sms_short_message = []
+for i, forecast in enumerate(data['list']):
+    temp_min = forecast['main']["temp_min"]
+    temp_max = forecast['main']["temp_max"]
+    weather_main = forecast['weather'][0]["main"]
+    weather_id = forecast['weather'][0]["id"]
+    dt_txt = forecast['dt_txt']
+
+    # Convert dt_txt to UTC datetime
+    utc_dt = datetime.fromisoformat(dt_txt)
+    utc_dt = pytz.UTC.localize(utc_dt)
+
+    # ✅ Convert to local time using timezone OBJECT
+    local_dt = utc_dt.astimezone(timezone_obj)
+
+    # ✅ Call strftime() on the datetime object
+    formatted_time = local_dt.strftime('%d/%m/%Y %H:%M:%S')
+    print(f"At {formatted_time}, Temperature: from {temp_min} to {temp_max}, Weather {weather_main}  ID= {weather_id}")
+
+    sms_short_message.append(f"At {formatted_time},{temp_min}oC to {temp_max}oC, {weather_main}")
+sms_short_weather_forecast = "\n".join(sms_short_message)
+print(sms_short_weather_forecast)
+
+# send an SMS with the weather prediction (message)
+#for twilio sms
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_=TwilioPhoneNumber,
+    body=sms_short_weather_forecast,
+    to=MyPhoneNumber,
+)
+
+print(message.status)
